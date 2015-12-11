28b75e91cd2169c4c021fd5982f677aec84ce152
Sébastien Vaucher <sebyx31@users.noreply.github.com>
2015-12-11 16:22:42 +0000

    Update log parsing script
    git-svn-id: https://subversion.cnds.unibe.ch/svn/hs15_sn_practical/wsn2015_group4@25 0f753b85-d2b3-4cde-b637-7fe495401850

--- parse_logs.py
+++ parse_logs.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""
+Log parser for the tree-based routing protocol program logs.
+
+Lecture of Sensor Networks and Internet of Things, Uni Bern
+Last modification: 11 December 2015
+Author: Sébastien Vaucher
+"""
 import os
 import xml
 from xml.sax.handler import ContentHandler
@@ -6,6 +13,7 @@ from xml.sax.handler import ContentHandler
 import re
 
 all_nodes = {}
+hop_count_distribution = []
 
 
 class Node:
@@ -78,6 +86,15 @@ class BuildTreeHandler(XmlHandler):
                 parent.add_child(self.node)
 
 
+class DistributionHandler(XmlHandler):
+    def characters(self, content):
+        global hop_count_distribution
+        if self.location == ['wiseml', 'trace', 'node', 'data']:
+            m = re.search('^stats (([0-9]+ ?)+)$', content)
+            if m is not None:
+                hop_count_distribution = [int(x) for x in m.group(1).split(' ') if x not in ('', '0')]
+
+
 def print_tree(node, level=0):
     print("|\t" * level, end='')
     print(node.nid)
@@ -85,29 +102,59 @@ def print_tree(node, level=0):
         print_tree(child, level + 1)
 
 
-def tikz_relations(node, file_handle):
+def tikz_relations(node, file_handle, color, phase):
     for child in node.children:
-        file_handle.write('\draw [red, thick, <-, >=latex] (%d,%d,%d) to (%d,%d,%d);' % (node.x, node.y, node.z, child.x, child.y, child.z))
-        tikz_relations(child, file_handle)
+        file_handle.write("\draw [%s, thick, dash pattern= on 4mm off 4mm, dash phase=%dmm, <-, >=latex] (%d,%d,%d) to (%d,%d,%d);\n" % (color, phase, node.x, node.y, node.z, child.x, child.y, child.z))
+        tikz_relations(child, file_handle, color, phase)
 
 
 def export_tikz(file_handle):
+    global all_nodes
+    print("Writing %s" % str(file_handle))
     file_handle.write(r"""
     \begin{tikzpicture}[scale=0.097,y= {(0.281cm,0.281cm)}, z={(0cm,1cm)}, x={(1cm,0cm)}]]
     \node[anchor=south west,inner sep=0] at (0,0) {\includegraphics[width=\textwidth]{3D_image_iam_complete.png}};
     """)
-    tikz_relations(all_nodes[1], file_handle)
+    phase = 2
+    for color, file in zip(('Maroon', 'JungleGreen'), filter(lambda x: re.match('logs/xmac_[a-z]+\.wiseml$', x), os.listdir('.'))):
+        print(file)
+        all_nodes = {}
+        xml.sax.parse(open(file), InstantiateNodesHandler())
+        xml.sax.parse(open(file), BuildTreeHandler())
+        tikz_relations(all_nodes[1], file_handle, color, phase)
+        phase += 4
+        
     for node in all_nodes.values():
         file_handle.write(r'\node [blue, very thick] (%d) at (%d,%d,%d) {$\bullet$};' % (node.nid, node.x, node.y, node.z))
     file_handle.write(r'\end{tikzpicture}')
 
 
+def output_distribution_stats(dist, file):
+    print("Stats for %s:" % file)
+    # Average hop-count
+    average_hc = sum([x[0] * x[1] for x in zip(dist, range(1, len(dist)))]) / sum(dist)
+    print("\tAverage hop-count = %f" % average_hc)
+
+    # Global delivery rate
+    gdr = sum(dist[1:]) / (len(all_nodes) - 1) / dist[0] * 100.0
+    print("\tGlobal delivery rate = %f %%" % gdr)
+
+    if re.match('xmac', str(file)):
+        # Connected delivery rate
+        non_connected_nodes = len([node for node in all_nodes.values() if node.parent is None])
+        cdr = sum(dist[1:]) / (len(all_nodes) - non_connected_nodes) / dist[0] * 100.0
+        print("\tConnected delivery rate = %f %%" % cdr)
+        cn_percent = (len(all_nodes) - non_connected_nodes) / len(all_nodes) * 100.0
+        print("\tConnected nodes = %f %%" % cn_percent)
+
 if __name__ == '__main__':
-    for file in filter(lambda x: re.match('xmac_[a-z]+\.wiseml$', x), os.listdir('.')):
-        print(file)
+    with open('report/map.tex', 'w') as tikz:
+        export_tikz(tikz)
+
+    for file in filter(lambda x: re.match('logs/.+\.wiseml$', x), os.listdir('.')):
         all_nodes = {}
+        hop_count_distribution = []
         xml.sax.parse(open(file), InstantiateNodesHandler())
         xml.sax.parse(open(file), BuildTreeHandler())
-        #print_tree(all_nodes[1])
-        with open('../report/%s.tex' % file, 'w') as tikz:
-            export_tikz(tikz)
+        xml.sax.parse(open(file), DistributionHandler())
+        output_distribution_stats(hop_count_distribution, file)
